f44235af2420cf27599d804f2d339904bc72244a
Alexandra <alexis07oana@yahoo.com>
2019-05-26 15:49:59 +0300

    Domain classes

---
+++ Application/ConferenceManagementSystem/Domain/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagementSystem.Entities
+{
+    class Author : User
+    {
+        private string Affiliation { get; set; }
+    }
+}
---
+++ Application/ConferenceManagementSystem/Domain/PCMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferenceManagementSystem.Entities
+{
+    class PCMember : User
+    {
+        private string Affiliation { get; set; }
+        private string website { get; set; }
+        private bool isReviewer { get; set; }
+    }
+}
