8170ad6c042e0ba2bdf3cda3929310d094b50db7
Alexandru <alexandru.simion013@gmail.com>
2019-05-26 15:06:51 +0300

    UI layout

--- Application/ConferenceManagementSystem/UserMainWindow.xaml.cs
+++ Application/ConferenceManagementSystem/UserMainWindow.xaml.cs
@@ -28,5 +28,10 @@ namespace ConferenceManagementSystem
         {
 
         }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
