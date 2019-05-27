e518017decbe11ad455be358a2ed0792eb63f9f1
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-05-27 07:17:17 +0300

    added email validation on PCMember register

--- Application/ConferenceManagementSystem/Controller/CMSController.cs
+++ Application/ConferenceManagementSystem/Controller/CMSController.cs
@@ -88,18 +88,23 @@ namespace ConferenceManagementSystem.Controller
         {
             List<String> res;
             List<String> res1;
+            List<String> res2;
             using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["cmsDatabase"].ConnectionString))
             {
                 res = db.Query<String>("SELECT FirstName FROM Users WHERE Username='" + username + "'").ToList();
                 res1 = db.Query<String>("SELECT FirstName FROM Users WHERE email='" + email + "'").ToList();
+                res2 = db.Query<String>("SELECT RoleID FROM ChosenPC WHERE email='" + email + "'").ToList();
                 if (res.Capacity > 0 || res1.Capacity > 0)
                     throw new Exception("Username/Email already in use");
+                if (res2.Capacity == 0)
+                    throw new Exception("You don't have the right to register as a PC Member");
                 else
                 {
-                    String query = "INSERT INTO Users(FirstName,LastName,Username,Passwd,email,RoleID) values('" + fname + "','" + lname + "','" + username + "','" + passwd + "','" + email + "',3)";
+                    int xv = Int32.Parse(res2[0]);
+                    String query = "INSERT INTO Users(FirstName,LastName,Username,Passwd,email,RoleID) values('" + fname + "','" + lname + "','" + username + "','" + passwd + "','" + email + "',"+xv+")";
                     db.Execute(query);
                     User user = db.QueryFirst<User>("SELECT * FROM Users WHERE Username='" + username + "' AND Passwd='" + passwd + "'");
-                    String query1 = "INSERT INTO PCMembers(ID,Affiliation,website,isReviewer) values(" + user.ID + ",'" + affiliation + "','"+website+"',0)";
+                    String query1 = "INSERT INTO PCMembers(ID,Affiliation,website,isReviewer) values(" + user.ID + ",'" + affiliation + "','" + website + "',0)";
                     db.Execute(query1);
                 }
             }
