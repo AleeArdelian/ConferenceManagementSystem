f0dc81bd1a6c40a7e0dedb2c4eb167d9a9c83a38
Alexandru <alexandru.simion013@gmail.com>
2019-05-29 17:51:33 +0300

    Added listview to superuser

--- Application/ConferenceManagementSystem/ConfManagementPage.xaml.cs
+++ Application/ConferenceManagementSystem/ConfManagementPage.xaml.cs
@@ -24,5 +24,10 @@ namespace ConferenceManagementSystem
         {
             InitializeComponent();
         }
+
+        private void ConferencesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
