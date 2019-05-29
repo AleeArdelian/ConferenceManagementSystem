3ec9d43911e481af78a875c1cc420d7576ed2190
Alexandru <alexandru.simion013@gmail.com>
2019-05-29 18:43:47 +0300

    Add conference implemented

--- Application/ConferenceManagementSystem/Controller/CMSController.cs
+++ Application/ConferenceManagementSystem/Controller/CMSController.cs
@@ -20,12 +20,8 @@ namespace ConferenceManagementSystem.Controller
             {
                 try
                 {
-                    String query = "INSERT INTO Papers(ContentLoc,AbstractLoc,Topic,PaperName,SectionID,isAccepted) VALUES ('" + ContentLoc + "','" + AbstractLoc + "','" + Topic + "','" + PaperName + "'," + SectionID + ",0)";
+                    String query = "INSERT INTO Conferences VALUES ('" + ConferenceName + "','" + ConferenceAddress + "','" + ConferenceDate + "')";
                     db.Execute(query);
-                    pid = db.Query<String>("SELECT ID FROM Papers WHERE ContentLoc='" + ContentLoc + "'").ToList();
-                    int pidd = Int32.Parse(pid[0]);
-                    String query1 = "INSERT INTO AuthorPapers(AuthorID,PaperID) VALUES (" + AuthorID + "," + pidd + ")";
-                    db.Execute(query1);
                 }
                 catch (Exception ex)
                 {
